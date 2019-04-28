8443835c383777486e8eada67ea94bca38a15996
Cavaco <nonuuxx@hotmail.com>
2019-04-28 20:47:41 +0100

    hash node

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
-
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
         System.out.println("test");
     }
 }
